f2863f8653be5307259526883e0d956c0b359436
qwexodn <sdfxodn@naver.com>
2017-06-16 17:43:39 +0900

    ㄴ

--- app/src/main/java/com/example/qwexo/weather_interface/MainActivity.java
+++ app/src/main/java/com/example/qwexo/weather_interface/MainActivity.java
@@ -17,7 +17,6 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseReference fireDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
